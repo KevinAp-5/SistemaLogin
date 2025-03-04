344b2fbe5790496e200ba53e4401cff334a2602f
KevinAp-5 <keven.clash3@gmail.com>
2025-03-04 00:42:30 -0300

    feat: User model/domain creation

---
+++ src/main/java/com/usermanager/manager/model/User.java
@@ -0,0 +1,27 @@
+package com.usermanager.manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "user")
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String login;
+    private String password;
+
+}
